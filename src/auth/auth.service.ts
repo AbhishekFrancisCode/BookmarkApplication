d88f25a5560c07dde9c3f08b4915602d27692201
abhishek <abhishek@finemake.in>
2023-08-17 11:18:28 +0530

    integrated docker, prisma and postgresql

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return 'i am signup';
+  }
+
+  signin() {
+    return 'i am signin';
+  }
+}
